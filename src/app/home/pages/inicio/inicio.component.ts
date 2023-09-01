7f1136b6fcd74c67a62f256b3caacf3450aac074
gutierrezcallazacadaniel2020 <gutierrezcallazacadaniel2020@gmail.com>
2023-09-01 12:10:31 -0500

    calendar init

--- src/app/home/pages/inicio/inicio.component.ts
+++ src/app/home/pages/inicio/inicio.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FullCalendarModule } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FullCalendarModule
+  ],
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.scss']
 })
